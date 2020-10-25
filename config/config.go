7191cad1cc6dbf4f8e2e948186b1235db1bac1b3
shadum <shariff_m@bridgeasiagroup.com>
2020-10-25 08:15:09 +0700

    read config from env file

--- config/config.go
+++ config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Config struct {
 	DB   *DBConfig
 	Auth *AuthReqConfig
@@ -21,19 +28,61 @@ type AuthReqConfig struct {
 }
 
 func GetConfig() *Config {
+	file, err := os.Open("/go/src/go-with-compose/.env")
+
+	m := make(map[string]string)
+	param := []string{"DB_HOST", "DB_PASSWORD", "DB_NAME", "DB_HOST_PORT", "AUTH_USER", "AUTH_PASSWORD", "AUTH_KEY"}
+
+	if err == nil {
+		scanner := bufio.NewScanner(file)
+
+		scanner.Split(bufio.ScanLines)
+		var text []string
+
+		for scanner.Scan() {
+			text = append(text, scanner.Text())
+		}
+
+		defer file.Close()
+
+		for _, eachLn := range text {
+			// println(strings.Split(eachLn, "=")[1])
+			paramChk := strings.Split(eachLn, "=")
+			for _, p := range param {
+				if paramChk[0] == p {
+					m[p] = paramChk[1]
+				}
+			}
+		}
+
+	} else {
+		m["DB_HOST"] = "192.168.1.107"
+		m["DB_PASSWORD"] = "12345"
+		m["DB_NAME"] = "mainDb"
+		m["DB_HOST_PORT"] = "9568"
+		m["AUTH_USER"] = "user"
+		m["AUTH_PASSWORD"] = "password"
+		m["AUTH_KEY"] = "my_secret_key"
+	}
+
+	dbPort, err := strconv.Atoi(m["DB_HOST_PORT"])
+	if err != nil {
+		dbPort = 9568
+	}
+
 	return &Config{
 		DB: &DBConfig{
 			Dialect:  "mysql",
-			Host:     "192.168.1.107",
-			Port:     9568,
+			Host:     m["DB_HOST"],
+			Port:     dbPort,
 			Username: "root",
-			Password: "12345",
-			Name:     "mainDb",
+			Password: m["DB_PASSWORD"],
+			Name:     m["DB_NAME"],
 		},
 		Auth: &AuthReqConfig{
-			Username: "user1",
-			Password: "password",
-			Jwtkey:   []byte("my_secret_key"),
+			Username: m["AUTH_USER"],
+			Password: m["AUTH_PASSWORD"],
+			Jwtkey:   []byte(m["AUTH_KEY"]),
 		},
 	}
 }
